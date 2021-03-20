2134ed133c878ae67d76c30e04431c06c834efba
qianpeisheng <qpeisheng@gmail.com>
2021-03-20 17:25:53 +0800

    add spell checker code and data

--- loca_test_v2_lang.py
+++ loca_test_v2_lang.py
@@ -21,13 +21,10 @@ import pandas as pd
 import datetime
 
 # Configs
-model_name = 'indobenchmark/indobert-base-p1' # indobenchmark/indobert-lite-base-p1'
+model_name = 'indobenchmark/indobert-lite-base-p1'
 max_seq_length = 167 # for train and test
 preprocessing_num_workers = 4
-batch_size=128 # depend on gpu memory
-test_batch_size=1024 # speed up
-
-tokenizer = BertTokenizer.from_pretrained(model_name) # make this global
+batch_size=2048 # depend on gpu memory
 
 # utils
 def get_pos(index1, index2, embedding, cls_):
@@ -49,8 +46,7 @@ class IndBert(nn.Module):
     def __init__(self):
         super(IndBert, self).__init__()
         model = AutoModel.from_pretrained(model_name)
-        # model.resize_token_embeddings(30521)
-        model.resize_token_embeddings(len(tokenizer))
+        model.resize_token_embeddings(30521)
 # https://github.com/huggingface/transformers/issues/4153
         self.bert = model
         self.linear = nn.Linear(in_features=768, out_features=4, bias=True)
@@ -171,7 +167,7 @@ class My_lm(pl.LightningModule):
                 street_loss += F.cross_entropy(embedding[index,:,2].unsqueeze(dim=0), street_start[index].unsqueeze(dim=0))
                 street_loss += F.cross_entropy(embedding[index,:,3].unsqueeze(dim=0), street_end[index].unsqueeze(dim=0))
 
-        total_loss = (cls_loss + poi_loss + street_loss)/3 # same as train loss     
+        total_loss = (cls_loss + poi_loss + street_loss)/3     
 
         # acc
         _, cls_ = torch.max(out_cls, dim=1)
@@ -181,14 +177,13 @@ class My_lm(pl.LightningModule):
         def get_acc(pred, gt):
             return torch.tensor(accuracy_score(pred.cpu(), gt.cpu()))
         val_accs = [get_acc(pred_poi_start, poi_start), get_acc(pred_poi_end, poi_end), get_acc(pred_street_start, street_start), get_acc(pred_street_end, street_end)]
-        avg_acc = sum(val_accs)/len(val_accs)
+        
         self.log('val_loss', total_loss, on_step=True)
         self.log('poi_start', val_accs[0], on_step=True)
         self.log('poi_end', val_accs[1], on_step=True)
         self.log('street_start', val_accs[2], on_step=True)
         self.log('street_end', val_accs[3], on_step=True)
-        self.log('avg_acc', avg_acc, on_step=True)
-        return {'val_loss': total_loss, 'poi_start': val_accs[0], 'poi_end': val_accs[1], 'street_start': val_accs[2], 'street_end': val_accs[3], 'avg_acc':avg_acc}
+        return {'val_loss': total_loss, 'poi_start': val_accs[0], 'poi_end': val_accs[1], 'street_start': val_accs[2], 'street_end': val_accs[3]}
         # may use F1 to measure the performance
     
     def validation_epoch_end(self, valid_step_outs):
@@ -200,20 +195,12 @@ class My_lm(pl.LightningModule):
             epoch_accs[1] += d['poi_end']
             epoch_accs[2] += d['street_start']
             epoch_accs[3] += d['street_end']
-        val_loss = epoch_val_loss/len(valid_step_outs)
-        avg_p_s = epoch_accs[0]/len(valid_step_outs)
-        avg_p_e = epoch_accs[1]/len(valid_step_outs)
-        avg_s_s = epoch_accs[2]/len(valid_step_outs)
-        avg_s_e = epoch_accs[3]/len(valid_step_outs)
-        avg_acc = sum([avg_p_s, avg_p_e, avg_s_s, avg_s_e])/4
-        self.log('val_loss', val_loss, on_epoch=True, prog_bar=True)
-        self.log('poi_start', avg_p_s, on_epoch=True, prog_bar=True)
-        self.log('poi_end', avg_p_e, on_epoch=True, prog_bar=True)
-        self.log('street_start', avg_s_s, on_epoch=True, prog_bar=True)
-        self.log('street_end', avg_s_e, on_epoch=True, prog_bar=True)
-        self.log('avg_acc', avg_acc, on_epoch=True)
-        return {'val_loss': val_loss, 'poi_start': avg_p_s, 'poi_end': avg_p_e, 'street_start': avg_s_s, 'street_end': avg_s_e, 'avg_acc':avg_acc}
-
+        self.log('val_loss', epoch_val_loss/len(valid_step_outs), on_epoch=True, prog_bar=True)
+        self.log('poi_start', epoch_accs[0]/len(valid_step_outs), on_epoch=True, prog_bar=True)
+        self.log('poi_end', epoch_accs[1]/len(valid_step_outs), on_epoch=True, prog_bar=True)
+        self.log('street_start', epoch_accs[2]/len(valid_step_outs), on_epoch=True, prog_bar=True)
+        self.log('street_end', epoch_accs[3]/len(valid_step_outs), on_epoch=True, prog_bar=True)
+        
     def test_step(self, batch, batch_idx):
         # batch
         input_ids = batch['input_ids']
@@ -227,7 +214,7 @@ class My_lm(pl.LightningModule):
         _, cls_ = torch.max(out_cls, dim=1)
         pred_poi_start, pred_poi_end = get_pos(0,1, embedding, cls_)
         pred_street_start, pred_street_end = get_pos(2,3, embedding, cls_)
-        tokenizer = BertTokenizer.from_pretrained(model_name) # make this global
+        tokenizer = BertTokenizer.from_pretrained("indobenchmark/indobert-base-p1") # make this global
         def decode_(pred_start, pred_end):   
             rets = []
             for index, (start, end) in enumerate(zip(pred_start, pred_end)):
@@ -239,7 +226,6 @@ class My_lm(pl.LightningModule):
                     current_input_ids = input_ids[index]
                     end = min(len(current_input_ids), end)
                     current_ret = tokenizer.decode(current_input_ids[start+1:end+1], skip_special_tokens=True)
-                    # NOTE [IMPORTANT] This +1 is the key to raise score from 0.01 to 0.5x
                 rets.append(current_ret)
             return rets
         pois = decode_(pred_poi_start, pred_poi_end)
@@ -264,9 +250,9 @@ class My_lm(pl.LightningModule):
 class Dm(pl.LightningDataModule):
     def __init__(self, batch_size=batch_size):
         super().__init__()
-        self.train_file = 'new_train_2.csv'
-        self.valid_file = 'new_train_2.csv'
-        self.test_file = 'new_test_2.csv'
+        self.train_file = 'train.csv'
+        self.valid_file = 'train.csv'
+        self.test_file = 'test.csv'
         self.batch_size = batch_size
       # When doing distributed training, Datamodules have two optional arguments for
       # granular control over download/prepare/splitting data:            
@@ -275,10 +261,10 @@ class Dm(pl.LightningDataModule):
     def setup(self, stage=None):
         # step is either 'fit', 'validate', 'test', or 'predict'. 90% of the time not relevant
         # load dataset
-        datasets = load_dataset('csv', data_files=self.train_file, split=['train[:100%]', 'train[80%:]']) # use all training data to prepare for final submission
-        column_names = ['id', 'raw_address', 'POI/street']
+        # datasets = load_dataset('csv', data_files='train.csv', split=['train[:80%]', 'train[80%:]'])
+        # column_names = ['id', 'raw_address', 'POI/street']
 
-        tokenizer = BertTokenizer.from_pretrained(model_name)
+        tokenizer = BertTokenizer.from_pretrained("indobenchmark/indobert-base-p1")
         #pad 0 https://huggingface.co/transformers/model_doc/bert.html
 
         def tokenize_fn(entry):
@@ -295,19 +281,7 @@ class Dm(pl.LightningDataModule):
 #                         return i+1, i+len(lst1)+1
                         return i, i+len(lst1) # [TODO] debug on this plus 1 due to splitting [CLS] at start of sequence
                 else:
-                    # find the **similar** sublist, slide and count common items, return the place with most common items
-                    # this method CAN mismatch
-                    num_commons = []
-                    for j in range(len(lst2)-len(lst1)+1):
-                        lst2_part = lst2[j:j+len(lst1)]
-                        temp_sum = sum([1 if lst2_part[k] == lst1[k] else 0 for k in range(len(lst2_part))])
-                        num_commons.append(temp_sum)
-                    if len(num_commons) == 0:
-                        # the lst2 is shorter than lst 1
-                        return(1, len(lst2)-1)
-                    max_ind = num_commons.index(max(num_commons))
-                    return (max_ind, max_ind+len(lst1))
-                    
+                    return (0, 0) # -1 triggers index out of bound error
             labels = entry['POI/street'].split('/')
             encoded_poi = tokenizer.encode(labels[0])
             entry_poi_pos = find_sublist(encoded_poi, encoded)
@@ -319,35 +293,36 @@ class Dm(pl.LightningDataModule):
             if labels[1]:
                 cls_label += 2
             return {'POI':entry_poi_pos, 'street': entry_street_pos, 'cls_label': cls_label}
-        datasets = [dataset.map(lambda entries: tokenizer(entries['raw_address'], padding=True), batched=True, batch_size=batch_size, num_proc=1) for dataset in datasets]
-        tokenized_d_train, tokenized_d_valid = [dataset.map(tokenize_fn, num_proc=preprocessing_num_workers) for dataset in datasets] # attempts to avoid size mismatch 
-        self.train_dataset = tokenized_d_train
-        self.valid_dataset = tokenized_d_valid
+        # datasets = [dataset.map(lambda entries: tokenizer(entries['raw_address'], padding=True), batched=True, batch_size=batch_size, num_proc=1) for dataset in datasets]
+        # tokenized_d_train, tokenized_d_valid = [dataset.map(tokenize_fn, num_proc=preprocessing_num_workers) for dataset in datasets] # attempts to avoid size mismatch 
+        # self.train_dataset = tokenized_d_train
+        # self.valid_dataset = tokenized_d_valid
         
         # test dataset
-        test_d = load_dataset('csv', data_files=self.test_file, split='train[:100%]') # adjust the ratio for debugging
-        tokenized_d_test = test_d.map(lambda entries: tokenizer(entries['raw_address'], padding=True), batched=True, batch_size=test_batch_size, num_proc=1)
+        # see how it performs on train
+        test_d = load_dataset('csv', data_files='test.csv', split='train[:100%]') # adjust the ratio for debugging
+        tokenized_d_test = test_d.map(lambda entries: tokenizer(entries['raw_address'], padding=True), batched=True, batch_size=batch_size, num_proc=1)
         self.test_dataset = tokenized_d_test# ['train'] # named by the dataset module
         
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=1)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=1, drop_last=True)
 
     def val_dataloader(self):
-        return DataLoader(self.valid_dataset, batch_size=self.batch_size, num_workers=1)
+        return DataLoader(self.valid_dataset, batch_size=self.batch_size, num_workers=1, drop_last=True)
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=test_batch_size, num_workers=1)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=1)
 
 checkpoint_callback = ModelCheckpoint(
-    # monitor='val_loss',
-    monitor='avg_acc',
-    dirpath='./exp1/',
-    filename='bert-ind-{epoch:03d}-{val_loss:.3f}-{avg_acc:.3f}',
-    save_top_k=100,
-    mode='max',
+    monitor='val_loss',
+    dirpath='.',
+    filename='bert-ind-{epoch:02d}-{val_loss:.2f}',
+    save_top_k=10,
+    mode='min',
 )
 
 dm = Dm()
+
 lm = My_lm()
 
 # # debug
@@ -357,13 +332,13 @@ lm = My_lm()
 # trainer = pl.Trainer(gpus=1, max_epochs=10, limit_train_batches=10, limit_val_batches=3, callbacks=[checkpoint_callback])
 
 # standard train, validation and test
-trainer = pl.Trainer(gpus=1, max_epochs=100, callbacks=[checkpoint_callback], stochastic_weight_avg=True, gradient_clip_val=1)
-trainer.fit(lm,dm)
-result = trainer.test()
+# trainer = pl.Trainer(gpus=1, max_epochs=50, callbacks=[checkpoint_callback])
+# trainer.fit(lm,dm)
+# result = trainer.test()
 
 # # testing only 
-# use larger batch size to speed up testing
-# dm.setup()
-# model = lm.load_from_checkpoint('bert-ind-epoch=00-val_loss=77.79.ckpt')
-# trainer = pl.Trainer(gpus=1)
-# result = trainer.test(model, test_dataloaders=dm.test_dataloader())
+# # use larger batch size to speed up testing
+dm.setup()
+model = lm.load_from_checkpoint('legend/bert-ind-epoch=039-val_loss=0.40.ckpt')
+trainer = pl.Trainer(gpus=1)
+result = trainer.test(model, test_dataloaders=dm.test_dataloader())
